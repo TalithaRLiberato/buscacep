4f79ad941d50bb5fe9d37ab46fa51ad353f8ecdf
Talitha Rodrigues <rodriguestalitha59@gmail.com>
2024-08-16 16:59:03 -0300

    primeira versão

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+//bloco de importações do código 
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './style.css';
+import api from "./services/api"
 
+//Função cria duas variáveis com diferentes comportamentos
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCEP] = useState({});
+
+  async function handleSearch(){
+    
+    //funcionam juntos
+    if(input === ''){
+      alert("Preencha algum CEP!")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`)
+      setCEP(response.data)
+      setInput("")
+    }catch{
+      alert("Erro ao buscar CEP!")
+      setInput("")
+    }
+  }
+  
+  //retorna a requisição, voltando a página em react
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Talitha Rodrigues</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu CEP..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
+
+      <button className="buttonSearch" onClick={handleSearch}>
+        <FiSearch size={25} color="#FFF"/>
+       </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>Rua: {cep.logradouro}</span>
+          <span>Complemento:{cep.complemento}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+        
+      </div>
   );
 }
 
+//Exporta a aplicação, torna a aplicação app pública
 export default App;
